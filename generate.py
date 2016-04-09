958a63d122a61e3646bfd9aa9253117721c8842c
dudongtai2002 <dudongtai@wireless1x-168-122-10-168.bu.edu>
2016-04-09 16:49:30 -0400

    添加缺失的ttf
    chfont

--- generate.py
+++ generate.py
@@ -13,12 +13,13 @@ import os
 from os.path import basename
 
 for file in os.listdir("Fonts/"):
-    font=ImageFont.truetype("Fonts/"+file,49)
-    im=Image.new("L",(49,49),"WHITE")
-    dr=ImageDraw.Draw(im)
-    dr.text((0,0),chr(0x6c38),font=font)
-    name="test/"+file[0:-4]+".png"
-    im.save(name)
+    if(file.endswith(".ttf")):
+        font=ImageFont.truetype("Fonts/"+file,49)
+        im=Image.new("L",(49,49),"WHITE")
+        dr=ImageDraw.Draw(im)
+        dr.text((0,0),chr(0x6c38),font=font)
+        name="test/"+file[0:-4]+".png"
+        im.save(name)
 
 
 def changes():
