db5a54bf3bdf6d9bdb6d1d36c61343dbc04961be
dudongtai2002 <dudongtai@wireless1x-168-122-10-168.bu.edu>
2016-04-09 15:07:58 -0400

    Done create testing
    Done create testing

--- generate.py
+++ generate.py
@@ -3,3 +3,19 @@ This file is used to generate characters.
 """
 
 __author__ = 'dudongtai'
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+import os
+from os.path import basename
+
+for file in os.listdir("Fonts/"):
+    switch
+    font=ImageFont.truetype("Fonts/"+file,49)
+
+    im=Image.new("L",(49,49),"WHITE")
+    dr=ImageDraw.Draw(im)
+    dr.text((0,0),chr(0x6c38),font=font)
+    name="test/"+file[0:-3]+".png"
+    im.save(name)
