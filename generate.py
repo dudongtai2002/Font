1409237481e3db45c683d573e5fa3e350ab88056
dudongtai2002 <dudongtai@wireless1x-168-122-10-168.bu.edu>
2016-04-09 15:13:02 -0400

    123
    123

--- generate.py
+++ generate.py
@@ -11,11 +11,9 @@ import os
 from os.path import basename
 
 for file in os.listdir("Fonts/"):
-    switch
     font=ImageFont.truetype("Fonts/"+file,49)
-
     im=Image.new("L",(49,49),"WHITE")
     dr=ImageDraw.Draw(im)
     dr.text((0,0),chr(0x6c38),font=font)
-    name="test/"+file[0:-3]+".png"
+    name="test/"+file[0:-4]+".png"
     im.save(name)
