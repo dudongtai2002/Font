703b43c1e6f52685b4b8156e31cda638b4f1b4e4
yinzhifan4202 <2420451183@qq.com>
2016-04-09 16:53:39 -0400

    AllCharWithAllTtf
    加上了时间线主干上的ttf后缀选择

--- generate.py
+++ generate.py
@@ -12,14 +12,31 @@ from PIL import ImageDraw
 import os
 from os.path import basename
 
+wordfile = open('test/ChineseCharacter.txt')
+word = wordfile.read()[1:]
+
+
 for file in os.listdir("Fonts/"):
-    font=ImageFont.truetype("Fonts/"+file,49)
-    im=Image.new("L",(49,49),"WHITE")
-    dr=ImageDraw.Draw(im)
-    dr.text((0,0),chr(0x6c38),font=font)
-    name="test/"+file[0:-4]+".png"
+    if (file.endswith(".ttf")):
+        if not os.path.exists("test/"+file[0:-4]+ '/'):
+            os.makedirs("test/"+file[0:-4]+ '/')
+        for x in word:
+            font=ImageFont.truetype("Fonts/"+file,49)
+            im=Image.new("L",(49,49),"WHITE")
+            dr=ImageDraw.Draw(im)
+            dr.text((0,0),x,font=font)
+            name="test/"+file[0:-4]+ '/' + x + ".png"
+            im.save(name)
+
+
+
+"""
+Testing
+for x in word:
+    font = ImageFont.truetype("Fonts/simsun.ttf", 49)
+    im = Image.new("L", (49, 49), "WHITE")
+    dr = ImageDraw.Draw(im)
+    dr.text((0, 0), x, font=font)
+    name = "test/" + 'simsun/' + x + ".png"
     im.save(name)
-
-
-def changes():
-    return
+"""
