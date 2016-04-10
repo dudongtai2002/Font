fe8bfd99024445750daffa97cd21c682f5a55a6e
yinzhifan4202 <2420451183@qq.com>
2016-04-09 20:38:51 -0400

    Trivial contribution 2
    Debug…and activation function.

--- fontneuronnetworkalpha1.py
+++ fontneuronnetworkalpha1.py
@@ -12,8 +12,8 @@ inputsize = 49*49
 lay1size = 100
 lay2size = 100
 inputsize += 1
-w1 = n.matrix[[r.uniform(0,0.01) for i in range(trainingsize)] for x in range(lay1size)]
-b1 = n.matrix[[r.uniform(0,0.01)] for i in range(trainingsize)]
+w1 = n.matrix([[r.uniform(0,0.01) for i in range(inputsize)] for x in range(lay1size)])
+b1 = n.matrix([[r.uniform(0,0.01)] for i in range(inputsize)])
 
 
 def xtoy(input, wi, bi):
@@ -21,17 +21,18 @@ def xtoy(input, wi, bi):
     return wi * input + bi
 
 
-def propagate(input, w, b)
+def propagate(input, w, b):
     if len(w) != len(b):
-        print("some len of w does not equal to some len of b")
+        print("Size of w does not equal to size len of b")
         return
-    for i in len(range(w)):
-        input = xtoy(input, wi[i], bi[i])
+    for i in range(len(w)):
+        input = xtoy(input, w[i], b[i])
+        input = activation(input)
     return input
 
 
 def activation(x):
     return 1/(1+ma.exp(x))
-
+activation = n.vectorize(activation)
 
 
